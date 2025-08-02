fe4a4cc710d24df7cafe814f146d5efcb35ec3f8
aracope <156398622+aracope@users.noreply.github.com>
2025-08-02 12:45:28 -0700

    Downvote now actually works.

--- original_code/Joke.js
+++ original_code/Joke.js
@@ -10,11 +10,11 @@ class Joke extends Component {
     return (
       <div className="Joke">
         <div className="Joke-votearea">
-          <button onClick={evt => vote(id, +1)}>
+          <button onClick={(evt) => vote(id, +1)}>
             <i className="fas fa-thumbs-up" />
           </button>
 
-          <button onClick={evt => vote(id, -1)}>
+          <button onClick={(evt) => vote(id, -1)}>
             <i className="fas fa-thumbs-down" />
           </button>
 
--- src/Joke.js
+++ src/Joke.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Joke.css";
 
 /** A single joke, along with vote up/down buttons. */
@@ -11,7 +11,7 @@ function Joke({ id, vote, votes, text }) {
           <i className="fas fa-thumbs-up" />
         </button>
 
-        <button onClick={() => vote(id - 1)}>
+        <button onClick={() => vote(id, -1)}>
           <i className="fas fa-thumbs-down" />
         </button>
         {votes}
