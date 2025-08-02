d62090cf87993e7795f54ff8043d6b04c0e7d0f0
aracope <156398622+aracope@users.noreply.github.com>
2025-08-02 12:22:52 -0700

    Copied original files into a new folder. Refactor Joke.js  to use functional components with hooks instead of class components.

---
+++ original_code/o.js
@@ -0,0 +1,106 @@
+import React from "react";
+import "./Joke.css";
+
+function Joke({ id, vote, votes, text }) {
+  return (
+    <div className="Joke">
+      <div className="Joke-votearea">
+        <button onClick={() => vote(id, +1)}>
+          <i className="fas fa-thumbs-up" />
+        </button>
+
+        <button onClick={() => vote(id, -1)}>
+          <i className="fas fa-thumbs-down" />
+        </button>
+
+        {votes}
+      </div>
+
+      <div className="Joke-text">{text}</div>
+    </div>
+  );
+}
+
+export default Joke;
+
+// JokeList.js
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Joke from "./Joke";
+import "./JokeList.css";
+
+function JokeList({ numJokesToGet = 5 }) {
+  const [jokes, setJokes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getJokes();
+  }, []);
+
+  async function getJokes() {
+    try {
+      let jokes = [];
+      let seenJokes = new Set();
+
+      while (jokes.length < numJokesToGet) {
+        let res = await axios.get("https://icanhazdadjoke.com", {
+          headers: { Accept: "application/json" }
+        });
+        let joke = res.data;
+
+        if (!seenJokes.has(joke.id)) {
+          seenJokes.add(joke.id);
+          jokes.push({ ...joke, votes: 0 });
+        } else {
+          console.log("duplicate found!");
+        }
+      }
+
+      setJokes(jokes);
+      setIsLoading(false);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  function generateNewJokes() {
+    setIsLoading(true);
+    getJokes();
+  }
+
+  function vote(id, delta) {
+    setJokes(jokes =>
+      jokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
+    );
+  }
+
+  const sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <i className="fas fa-4x fa-spinner fa-spin" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="JokeList">
+      <button className="JokeList-getmore" onClick={generateNewJokes}>
+        Get New Jokes
+      </button>
+
+      {sortedJokes.map(j => (
+        <Joke
+          text={j.joke}
+          key={j.id}
+          id={j.id}
+          votes={j.votes}
+          vote={vote}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default JokeList;
